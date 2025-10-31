b1d17ad081267d988d17816a66266f59c52c7e6c
PZupo <114028453+PZupo@users.noreply.github.com>
2025-10-31 17:33:25 -0300

    Update index.tsx

--- kriative-social-studio Deploy/index.tsx
+++ kriative-social-studio Deploy/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import KriativeHeader from './components/KriativeHeader'
+import './index.css'
 
-// Renderiza o App
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <KriativeHeader appName="Social Studio" />
-    <App />
+    <KriativeHeader />
+    <div style={{ paddingTop: '80px' }}>
+      <App />
+    </div>
   </React.StrictMode>
 )
