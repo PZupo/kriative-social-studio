fff3bfa6d52075b16ce2441c8474fcb9f8a4e11f
PZupo <114028453+PZupo@users.noreply.github.com>
2025-10-31 21:07:31 -0300

    Update vite.config.ts

--- kriative-social-studio Deploy/vite.config.ts
+++ kriative-social-studio Deploy/vite.config.ts
@@ -3,4 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: ['/assets/index-C75-rJQP.js'], // Ignora o asset quebrado
+    },
+  },
+  assetsInclude: ['**/*.js'], // Inclui assets JS externos
 })
